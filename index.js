39ec55296190fec48709f4c19bc3e059d7de255b
masud <masudranaacb@gmail.com>
2024-08-16 16:35:31 +0600

    server and mongodb database setup some product load in products collection

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = 5000;
+
+// middelware
+
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(express.json());
+app.use(cors(corsOptions));
+
+
+
+// create database
+const uri = process.env.BD_URL;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const MyTechDB = client.db("MyTechDB");
+    const productsCollection = MyTechDB.collection("products");
+
+    app.get("/products",async(req,res)=>{
+        
+        const products=await productsCollection.find().toArray()
+        res.send(products)
+    })
+
+
+
+
+
+
+
+
+
+    console.log("connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+   
+  }
+}
+run().catch(console.dir);
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
