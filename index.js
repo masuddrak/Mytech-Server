3e6477229cee16750ab44f5ed3f862eae0375d46
masud <masudranaacb@gmail.com>
2024-08-17 19:58:58 +0600

    sort data low hi

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 5000;
 // middelware
 
 const corsOptions = {
-  origin: ["http://localhost:5173","https://trackshop-e2f38.web.app"],
+  origin: ["http://localhost:5173", "https://trackshop-e2f38.web.app"],
   credentials: true,
   optionSuccessStatus: 200,
 };
@@ -39,36 +39,49 @@ async function run() {
       const page = parseInt(req.query.page);
       const size = parseInt(req.query.size);
 
-      const { searchText, maxPrice, minPrice,category ,brand_name} = req.query;
-      console.log(searchText, maxPrice,category,brand_name);
+      const {
+        searchText,
+        maxPrice,
+        minPrice,
+        category,
+        brand_name,
+        sortPrice,
+      } = req.query;
+      console.log(searchText, maxPrice, category, brand_name, sortPrice);
       const query = {
         name: {
           $regex: searchText,
           $options: "i",
         },
       };
-     
+      //  sort low to hi
+      if (sortPrice=="low") {
+        const result = await productsCollection.find().sort( { "price": 1 } ).toArray()
+        return res.send(result);
+      }
       // max and minimum price sorting
-      if (minPrice>0 && maxPrice<20000) {
+      if (minPrice > 0 && maxPrice < 20000) {
         const result = await productsCollection
-          .find({ price: { $gte: parseInt(minPrice), $lte: parseInt(maxPrice) } })
+          .find({
+            price: { $gte: parseInt(minPrice), $lte: parseInt(maxPrice) },
+          })
           .toArray();
-      return  res.send(result);
+        return res.send(result);
       }
       // search products
       if (searchText) {
         const products = await productsCollection.find(query).toArray();
         return res.send(products);
       }
-       // load product category basis
-       if(category){
-        const result=await productsCollection.find({category}).toArray()
-        return res.send(result)
+      // load product category basis
+      if (category) {
+        const result = await productsCollection.find({ category }).toArray();
+        return res.send(result);
       }
-       // load product brand basis
-       if(brand_name){
-        const result=await productsCollection.find({brand_name}).toArray()
-        return res.send(result)
+      // load product brand basis
+      if (brand_name) {
+        const result = await productsCollection.find({ brand_name }).toArray();
+        return res.send(result);
       }
       // get all products
       const products = await productsCollection
@@ -83,7 +96,6 @@ async function run() {
       const count = await productsCollection.estimatedDocumentCount();
       res.send({ count });
     });
-    
 
     console.log("connected to MongoDB!");
   } finally {
